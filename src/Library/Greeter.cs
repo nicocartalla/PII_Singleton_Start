a7031565bbb7d4e2bedba3a18946038f89ba9649
Nicolas Cartalla <nicolas.cartalla@nexa.com.uy>
2020-09-25 23:13:14 -0300

    Tipos Genericos a Singleton

--- src/Library/Greeter.cs
+++ src/Library/Greeter.cs
@@ -4,26 +4,6 @@ namespace Library
 {
     public class Greeter
     {
-        private Greeter()
-        {
-            // Intentionally left blank
-        }
-
-        private static Greeter instance;
-
-        public static Greeter Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Greeter();
-                }
-
-                return instance;
-            }
-        }
-
         public void SayHiToTheWorld()
         {
             Console.WriteLine("Hello World!");
---
+++ src/Library/Singleton.cs
@@ -0,0 +1,25 @@
+namespace Library
+{
+    public class Singleton<T> where T : new()
+    {
+        private Singleton()
+        {
+            // Intentionally left blank
+        }
+
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+
+       
+    }
+}
